f66ec284fcfef9061bea8a44e1ef820459015165
Wesley <wesley.barros@compasso.com.br>
2021-09-11 18:43:04 -0300

    GruposdeEstilo

--- App.js
+++ App.js
@@ -1,25 +1,36 @@
-import React, {useState} from 'react';
-import { View, Text, Button} from 'react-native';
+import React from 'react';
+import { View, Text, StyleSheet} from 'react-native';
 
 function App(){
-  const [name, setName] = useState(''); //[] <- Nome do estado, A função para o estado.  () <- Valor padrão.
-  const [age, setAge] = useState('');
-function change(name, age){
-  setName(name);
-  setAge(age);
-}
-function clean(){
-  setAge('');
-  setName('');
-}
+
   return(
-    <View style={{alignItems: 'center', marginTop:25}}>
-      <Text>{name}</Text>
-      <Text>{age}</Text>
-      <Button title="Change name" onPress={ () =>change('Wessh', 25)} />
-      <Button title="Clean" onPress={clean} />
+    <View style={styles.area}>
+      <Text style={[styles.title, styles.tlign]}>Wessh</Text>
+      <Text style={styles.tlign}>Texto escrito aqui!</Text>
+      <Text style={styles.obs}>Mini texto!</Text>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  area:{
+    marginTop: 60,
+  },
+  title:{
+    fontSize: 20,
+    fontWeight: 'bold',
+    fontFamily: 'monospace',
+    color: '#FF7100',
+  },
+  tlign:{
+    textAlign: 'center'
+  },
+  obs:{
+    alignItems: 'baseline',
+    textAlign: 'right',
+    color: '#00FF00',
+    fontSize: 5
+  }
+});
 
 export default App;
