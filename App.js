4f0a04547aeae8ef1189737a70ede7858af08642
Wesley <wesley.barros@compasso.com.br>
2021-09-11 17:12:56 -0300

    ContinuacaoAnterior

--- App.js
+++ App.js
@@ -3,12 +3,13 @@ import { View, Text, Image } from 'react-native';
 
 function App(){
   let name = 'Wessh';
+  let image = 'https://pbs.twimg.com/media/Exwadx7W8Acct5F.jpg';
   return(
     <View style={{alignItems: 'center'}}>
       <View style={{ backgroundColor: '#FF7100', height: 25, width: 25, margin: 15}}></View>
 
       <Image
-        source={{ uri: 'https://pbs.twimg.com/media/Exwadx7W8Acct5F.jpg'}}
+        source={{ uri: image}}
         style={{ width: 300, height: 300}}
       />
       <Text style={{ color: '#FF7100', fontSize: 25, margin: 15 }}> {name} </Text>
