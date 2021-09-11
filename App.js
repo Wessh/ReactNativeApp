7408aaf13acf99b09cb791d934bfcbaafb344cee
Wesley <wesley.barros@compasso.com.br>
2021-09-11 16:29:54 -0300

    Basico{Texto,Imagem}

--- App.js
+++ App.js
@@ -4,19 +4,14 @@ import { View, Text, Image } from 'react-native';
 function App(){
   let name = 'Wessh';
   return(
-    <View>
-      <Text>
-        Mendix!!
-      </Text>
-      <Text style={{ color: '#FF7100', fontSize: 25, margin: 15 }}>
-        Ele é legal!!
-      </Text>
+    <View style={{alignItems: 'center'}}>
       <View style={{ backgroundColor: '#FF7100', height: 25, width: 25, margin: 15}}></View>
 
       <Image
-        source={{ uri: 'https://cdn.discordapp.com/attachments/881892744979701780/886047138981773323/FB_IMG_1631320454883.jpg'}}
+        source={{ uri: 'https://pbs.twimg.com/media/Exwadx7W8Acct5F.jpg'}}
         style={{ width: 300, height: 300}}
       />
+      <Text style={{ color: '#FF7100', fontSize: 25, margin: 15 }}> {name} </Text>
     </View>
   );
 }
