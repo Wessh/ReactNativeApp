723a720516549a35f0a9c7f848e49798d600307f
Wesley <wesley.barros@compasso.com.br>
2021-09-12 00:01:18 -0300

    TamanhosDinamicoseFixosEx1

--- App.js
+++ App.js
@@ -1,36 +1,20 @@
 import React from 'react';
-import { View, Text, StyleSheet} from 'react-native';
+import { View, Text} from 'react-native';
 
 function App(){
 
   return(
-    <View style={styles.area}>
-      <Text style={[styles.title, styles.tlign]}>Wessh</Text>
-      <Text style={styles.tlign}>Texto escrito aqui!</Text>
-      <Text style={styles.obs}>Mini texto!</Text>
+    //flex vai ocupar toda a tela!
+    <View style={{flex: 1, backgroundColor:'#121212'}}> 
+      <View style={{backgroundColor:'#121212', height:65}}></View>
+
+      <View style={{backgroundColor:'#DDD', flex: 1}}>
+        <Text>Wessh</Text>
+        <Text>Sub, texto aqui!</Text>
+      </View>
+
+      <View style={{backgroundColor:'#121212', height:65}}></View>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  area:{
-    marginTop: 60,
-  },
-  title:{
-    fontSize: 20,
-    fontWeight: 'bold',
-    fontFamily: 'monospace',
-    color: '#FF7100',
-  },
-  tlign:{
-    textAlign: 'center'
-  },
-  obs:{
-    alignItems: 'baseline',
-    textAlign: 'right',
-    color: '#00FF00',
-    fontSize: 5
-  }
-});
-
 export default App;
